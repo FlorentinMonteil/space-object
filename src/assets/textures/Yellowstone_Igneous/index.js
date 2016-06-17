f8d793915855248a1482b25d506427ae9a7add15
FlorentinMonteil <florentin.monteil@gmail.com>
2016-06-17 19:16:19 +0200

    Remove tex

--- src/assets/textures/Yellowstone_Igneous/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "AO": require('./T_Yellowstone_Igneous_AO.png'),
-  "BC": require('./T_Yellowstone_Igneous_BC.png'),
-  "H": require('./T_Yellowstone_Igneous_H.png'),
-  "N": require('./T_Yellowstone_Igneous_N.png')
-}
